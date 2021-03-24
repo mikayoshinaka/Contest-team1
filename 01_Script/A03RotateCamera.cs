9d2389617ae02c4ebed5576d80b6331b94f88eb5
L-SimoLieF <72783586+L-SimoLieF@users.noreply.github.com>
2021-03-24 10:01:54 +0900

    操作系のスクリプト群です。
    基本的には送ってもらったコードをそのまま使ってます。(そのまま使えたので)
    速度調整等は行っていません。また、カメラ操作に関しても未実装です。

---
+++ 01_Script/A03RotateCamera.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class A03RotateCamera : MonoBehaviour
+{
+    public GameObject targetObject;
+    Vector3 initiateDistance;
+    // Start is called before the first frame update
+    void Start()
+    {
+        initiateDistance = transform.position - targetObject.transform.position;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.position = targetObject.transform.position + initiateDistance;
+
+        if(Input.GetAxis("Horizontal2") >= 1)
+        {
+            transform.RotateAround(targetObject.transform.position, new Vector3(0, 1, 0), 0.5f);
+        }
+        if (Input.GetAxis("Horizontal2") <= -1)
+        {
+            transform.RotateAround(targetObject.transform.position, new Vector3(0, 1, 0), -0.5f);
+        }
+    }
+}
