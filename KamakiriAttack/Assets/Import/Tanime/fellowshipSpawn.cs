688ef52ac7de8908bb55b2817cc56f6b215ce482
iwamasa110 <g1927011@st.t-kougei.ac.jp>
2021-03-30 09:29:30 +0900

    KamakiriAttackの方が最新版のプロトタイプです 元のプロトタイプは一応残してあります オブジェクトのサイズや配置、キャラクターの移動速度などを調整 PlayerControllerとfellowshipSpawnを新しいものに差し替えました

--- KamakiriAttack/Assets/Import/Tanime/fellowshipSpawn.cs
+++ KamakiriAttack/Assets/Import/Tanime/fellowshipSpawn.cs
@@ -4,12 +4,12 @@ using UnityEngine;
 
 public class fellowshipSpawn : MonoBehaviour
 {
-   
+
     GameObject object1, object2, object3, object4, followobject1, followobject2, followobject3, followobject4;
-    float distance1, distance2, distance3, distance4, StopRange = 5f,spawnTime;
+    float distance1, distance2, distance3, distance4, StopRange = 5f, spawnTime, width1, width2;
     Vector3 direction1, direction2, direction3, direction4;
     RaycastHit RH;
-    int num = 50,newNum;
+    int num = 50, newNum;
     //判定サークルのオブジェクト取得
     [SerializeField] GameObject circle1 = default;
     [SerializeField] GameObject circle2 = default;
@@ -18,10 +18,10 @@ public class fellowshipSpawn : MonoBehaviour
     [SerializeField] GameObject player = default;
     //味方
     [SerializeField] GameObject friendParent = default;
-   
-    
+
+
     //リスポーン時使用変数
-    bool follow1, follow2, follow3, follow4, limit,start=false;
+    bool follow1, follow2, follow3, follow4, limit, start = false;
     public static bool startSpawn;
     //リスポーン対象:花(0):キノコ(1):木(2)
     //public static int spawnKind=1;
@@ -35,7 +35,7 @@ public class fellowshipSpawn : MonoBehaviour
     void Start()
     {
         startSpawn = true;
-        
+
     }
 
     void Update()
@@ -44,22 +44,37 @@ public class fellowshipSpawn : MonoBehaviour
         {
             limit = false;
         }
+        // 半径取得
+        width1 = circle1.GetComponent<Renderer>().bounds.size.x / 2;
+        width2 = circle2.GetComponent<Renderer>().bounds.size.x / 2;
 
-        //円の判定（仮）
-        //if ((circle1.transform.position.x - 2.0f <= this.transform.position.x && circle1.transform.position.x + 2.0f >= this.transform.position.x &&
-        //    circle1.transform.position.z - 2.0f <= this.transform.position.z && circle1.transform.position.z + 2.0f >= this.transform.position.z) ||
-        //    (circle2.transform.position.x - 2.0f <= this.transform.position.x && circle2.transform.position.x + 2.0f >= this.transform.position.x &&
-        //    circle2.transform.position.z - 2.0f <= this.transform.position.z && circle2.transform.position.z + 2.0f >= this.transform.position.z) && SurroundScript.END)
-        //    start = true;
+        //円の判定
+
+        float C1judge1, C1judge2, C1judge3, C1judge4, C2judge1, C2judge2, C2judge3, C2judge4;
+        C1judge1 = circle1.transform.position.x - width1;
+        C1judge2 = circle1.transform.position.x + width1;
+        C1judge3 = circle1.transform.position.z - width1;
+        C1judge4 = circle1.transform.position.z + width1;
+        C2judge1 = circle2.transform.position.x - width2;
+        C2judge2 = circle2.transform.position.x + width2;
+        C2judge3 = circle2.transform.position.z - width2;
+        C2judge4 = circle2.transform.position.z + width2;
+
+        if (circle1.activeSelf && circle2.activeSelf)
+            if ((C1judge1 <= this.transform.position.x && C1judge2 >= this.transform.position.x &&
+                C1judge3 <= this.transform.position.z && C1judge4 >= this.transform.position.z) ||
+                (C2judge1 <= this.transform.position.x && C2judge2 >= this.transform.position.x &&
+                C2judge3 <= this.transform.position.z && C2judge4 >= this.transform.position.z) && SurroundScript.END)
+                start = true;
 
 
         // アスビ用 (仮)  一応円の判定をGameObjectにします
 
-                    // DetectInsideScript と　InsideTriggerScript　利用
-                    if (DetectInsideScript.start)
-                    {
-                        start = true;   // 再生確認します、バグあり
-                    }
+        // DetectInsideScript と　InsideTriggerScript　利用
+        //if (DetectInsideScript.start)
+        //            {
+        //                start = true;   // 再生確認します、バグあり
+        //            }
 
         // ここまでアスビ用
 
@@ -75,11 +90,12 @@ public class fellowshipSpawn : MonoBehaviour
                 Vector3 spawnPos;
                 spawnPos = transform.position;
                 //対象のアニメーションを取得
-                animator=this.GetComponent<Animator>();
-                
-               
+                animator = this.GetComponent<Animator>();
 
-                if (!limit) {
+
+
+                if (!limit)
+                {
                     //花の再生開始
                     animator.SetBool("bloomMotion", true);
                     follow1 = true;
@@ -92,7 +108,7 @@ public class fellowshipSpawn : MonoBehaviour
                 }
                 spawnTime -= Time.deltaTime;
                 //スポーン処理
-                if (TemporaryFriendScript.friendCount == newNum&&spawnTime<2.0f)
+                if (TemporaryFriendScript.friendCount == newNum && spawnTime < 2.0f)
                 {   //1
                     object1 = friendParent.transform.Find("Cube (" + TemporaryFriendScript.friendCount + ')').gameObject;
                     followobject1 = friendParent.transform.Find("Cube (" + (TemporaryFriendScript.friendCount - 1) + ')').gameObject;
@@ -100,7 +116,7 @@ public class fellowshipSpawn : MonoBehaviour
                     TemporaryFriendScript.friendCount++;
                     object1.transform.position = this.transform.position + new Vector3(0, 2, 0);
                 }
-                if (TemporaryFriendScript.friendCount == newNum+1 && spawnTime < 1.8f)
+                if (TemporaryFriendScript.friendCount == newNum + 1 && spawnTime < 1.8f)
                 {   //2
                     object2 = friendParent.transform.Find("Cube (" + TemporaryFriendScript.friendCount + ')').gameObject;
                     object2.SetActive(true);
@@ -114,7 +130,7 @@ public class fellowshipSpawn : MonoBehaviour
                     TemporaryFriendScript.friendCount++;
                     object3.transform.position = this.transform.position + new Vector3(0, 1, 2);
                 }
-                
+
 
             }
             if (this.gameObject.CompareTag("mushroom"))
@@ -124,7 +140,7 @@ public class fellowshipSpawn : MonoBehaviour
                 spawnPos = transform.position;
                 //対象のアニメーションを取得
                 animator = this.GetComponent<Animator>();
-               
+
                 if (!limit)
                 {
                     //キノコの再生
--- KamakiriAttack/Assets/Script/PlayerController.cs
+++ KamakiriAttack/Assets/Script/PlayerController.cs
@@ -5,48 +5,51 @@ using UnityEngine;
 public class PlayerController : MonoBehaviour
 {
     public float speed = 1.0f;
-	Vector3 latestPos;
+    float gear = 260.0f;
+    Vector3 latestPos;
     // Start is called before the first frame update
     void Start()
     {
-		latestPos = transform.position;
+        latestPos = transform.position;
     }
 
     // Update is called once per frame
     void Update()
     {
+        Debug.Log("speed" + speed * (float)Time.deltaTime * gear * new Vector3(0.1f, 0, 0));
 
-		if (Input.GetAxis("Horizontal") > 0)
-		{
-			transform.position += speed * new Vector3(0.1f, 0, 0);
-			//moving = true;
-			
-		}
-		if (Input.GetAxis("Horizontal") < 0)
-		{
-			transform.position += speed * new Vector3(-0.1f, 0, 0);
-			//moving = true;
-			
-		}
-		if (Input.GetAxis("Vertical") > 0)
-		{
-			transform.position += speed * new Vector3(0, 0, 0.1f);
-			//moving = true;
-			
-		}
-		if (Input.GetAxis("Vertical") < 0)
-		{
-			transform.position += speed * new Vector3(0, 0, -0.1f);
-			//moving = true;
-			
-		}
 
-		Vector3 diff = transform.position - latestPos;   //前回からどこに進んだかをベクトルで取得
-		latestPos = transform.position;  //前回のPositionの更新
+        if (Input.GetAxis("Horizontal") > 0)
+        {
+            transform.position += speed * (float)Time.deltaTime * gear * new Vector3(0.1f, 0, 0);
+            //moving = true;
 
-		if (diff.magnitude > 0.01f)
-		{
-			transform.rotation = Quaternion.LookRotation(diff); //向きを変更する
-		}
-	}
+        }
+        if (Input.GetAxis("Horizontal") < 0)
+        {
+            transform.position += speed * (float)Time.deltaTime * gear * new Vector3(-0.1f, 0, 0);
+            //moving = true;
+
+        }
+        if (Input.GetAxis("Vertical") > 0)
+        {
+            transform.position += speed * (float)Time.deltaTime * gear * new Vector3(0, 0, 0.1f);
+            //moving = true;
+
+        }
+        if (Input.GetAxis("Vertical") < 0)
+        {
+            transform.position += speed * (float)Time.deltaTime * gear * new Vector3(0, 0, -0.1f);
+            //moving = true;
+
+        }
+
+        Vector3 diff = transform.position - latestPos;   //前回からどこに進んだかをベクトルで取得
+        latestPos = transform.position;  //前回のPositionの更新
+
+        if (diff.magnitude > 0.01f)
+        {
+            transform.rotation = Quaternion.LookRotation(diff); //向きを変更する
+        }
+    }
 }
